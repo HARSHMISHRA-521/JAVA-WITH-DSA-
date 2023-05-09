249411172f39c2273d7704b55c78f3bf2c841564
HARSH <mishraharshmishra2020@gmail.com>
2023-05-10 04:18:52 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH9_RECURSION/sum_first_n_naturalNo.java
@@ -0,0 +1,25 @@
+package CH9_RECURSION;
+//both space and time complexity are O(n)
+
+public class sum_first_n_naturalNo {
+    public static int sum(int n){
+        if(n == 1){
+            return 1;
+        }
+        if(n!=1) {
+            System.out.print(n + " + ");
+        }
+        else {
+            System.out.print(n + " = ");
+        }
+
+        int f = n + sum(n-1);
+
+        return f;
+    }
+
+    public static void main(String[] args) {
+        int n =5;
+        System.out.print(sum(n));
+    }
+}
