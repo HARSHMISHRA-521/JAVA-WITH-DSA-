c44114a69a555b1ce28a75ed61b993217c01bb22
HARSH <mishraharshmishra2020@gmail.com>
2023-05-10 01:35:12 +0530

    DSA CHAPTERWISE

--- is_bit_power_of_two.java
+++
@@ -1,9 +0,0 @@
-public class is_bit_power_of_two {
-    public static boolean isBitPowerOfTwo(int n){
-        return (n&(n-1))==0;   // because 2^n & 2^(n-1) == 0
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isBitPowerOfTwo(16));
-    }
-}
