dd0f3ec83e135991f538349ea43250ea0b1e4955
HARSH <mishraharshmishra2020@gmail.com>
2023-05-10 17:42:59 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH9_RECURSION/lastOccurance_array_trecursion.java
@@ -0,0 +1,20 @@
+package CH9_RECURSION;
+//time and space complexity is O(n)
+public class lastOccurance_array_trecursion {
+    public static int lastOcc(int []arr,int key,int i){
+        if(i==arr.length){
+            return -1;
+        }
+        int isfound = lastOcc(arr,key,i+1);
+        if(isfound == -1 & arr[i]==key){
+            return i;
+        }
+        return isfound;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {8,3,6,9,5,10,2,5,3};
+        int key = 5;
+        System.out.println(lastOcc(arr,key,0));
+    }
+}
