2c6cd67988d5e295d7a4f40c937eca659dfdf3e1
HARSH <mishraharshmishra2020@gmail.com>
2023-05-10 16:47:03 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH9_RECURSION/fibonacci_recursion.java
@@ -0,0 +1,18 @@
+package CH9_RECURSION;
+//its space complexity is O(n)
+//its time complexity is exponential ,i.e,O(2^n)
+
+public class fibonacci_recursion {
+    public static int fib(int n){
+        if(n==0 || n== 1){
+            return n;
+        }
+        int fib = fib(n-1) + fib(n-2);
+        return fib;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(fib(n));
+    }
+}
