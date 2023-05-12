05f808e347fb07f5288b69d350087efce25de322
HARSH <mishraharshmishra2020@gmail.com>
2023-05-12 17:58:09 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH9_RECURSION/Assignment_qNo_3.java
@@ -0,0 +1,17 @@
+//Question 3 : Write a program to find Length of a string using Recursion.
+
+package CH9_RECURSION;
+
+public class Assignment_qNo_3 {
+    public static int length(String str){
+        if(str.length()==0){
+            return 0;
+        }
+
+        return length(str.substring(1))+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("length of  string is "+length("harsh mishra"));
+    }
+}
