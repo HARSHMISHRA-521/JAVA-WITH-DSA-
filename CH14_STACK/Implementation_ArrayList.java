dee4efc4922b49b46d4005eebf10c12e27e2eb8c
HARSH <mishraharshmishra2020@gmail.com>
2023-06-10 02:01:52 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH14_STACK/Implementation_ArrayList.java
@@ -0,0 +1,41 @@
+package CH14_STACK;
+
+import java.util.ArrayList;
+
+public class Implementation_ArrayList {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+        //push
+        public static void push(int data){
+            list.add(data);
+        }
+        //pop
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top= list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        //peek
+        public static int peek(){
+            return list.get(list.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
