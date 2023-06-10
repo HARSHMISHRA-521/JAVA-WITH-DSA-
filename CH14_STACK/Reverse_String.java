39d03c3eaf8f47176f6c0adbf34c0478756513d3
HARSH <mishraharshmishra2020@gmail.com>
2023-06-10 16:24:00 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CH14_STACK/Reverse_String.java
@@ -0,0 +1,25 @@
+package CH14_STACK;
+import java.util.*;
+
+public class Reverse_String {
+    public static String reverseString(String str){
+       Stack<Character> s =new Stack<>();
+       int idx =0;
+       while(idx<str.length()){
+           s.push(str.charAt(idx));
+           idx++;
+       }
+       StringBuilder result= new StringBuilder("");
+       while(!s.isEmpty()){
+           char curr = s.pop();
+           result.append(curr);
+       }
+       return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String str  = "reversed";
+        String result = reverseString(str);
+        System.out.println(result);
+    }
+}
