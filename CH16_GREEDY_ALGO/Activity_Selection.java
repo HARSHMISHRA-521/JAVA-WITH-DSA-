110407180360c612924005c082aa5ad8bfb7d1b1
HARSH <mishraharshmishra2020@gmail.com>
2023-07-11 17:58:58 +0530

    DSA CHAPTERWISE QUESTIONS

--- CH16_GREEDY_ALGO/Activity_Selection.java
+++ CH16_GREEDY_ALGO/Activity_Selection.java
@@ -43,3 +43,53 @@ public class Activity_Selection {   // TC: O(n)
 //A0 A1 A3 A4
 
 
+// For Case : When the end time is not already sorted ,then we have to code for sorting it too:
+//So,here we go with the required changes ->
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Comparator;
+//
+//public class Activity_Selection {
+//    public static void main(String[] args) {
+//        int start[] = {0, 1, 3, 5, 5, 8};
+//        int end[] = {6, 2, 4, 7, 9, 9};
+//
+//        // Sorting
+//        int activities[][] = new int[start.length][3];
+//
+//        for (int i = 0; i < start.length; i++) {
+//            activities[i][0] = i;
+//            activities[i][1] = start[i];
+//            activities[i][2] = end[i];
+//        }
+//
+//        // Sort based on end time using lambda function
+//        Arrays.sort(activities, Comparator.comparingInt(o -> o[2]));
+//        //In java comparator are the interfaces for sorting Java objects
+//        //in the above line ,it will sort the 2d array on the basis of the " column 2" in ascending order(o->o[2])
+//
+//        int maxActivities = 0;
+//        ArrayList<Integer> selectedActivities = new ArrayList<>();
+//
+//        // Select the activities
+//        selectedActivities.add(activities[0][0]);
+//        int lastEndTime = activities[0][2];
+//
+//        for (int i = 1; i < end.length; i++) {
+//            if (activities[i][1] >= lastEndTime) {
+//                maxActivities++;
+//                selectedActivities.add(activities[i][0]);
+//                lastEndTime = activities[i][2];
+//            }
+//        }
+//
+//        System.out.println("Max activities: " + (maxActivities + 1));
+//        for (int i = 0; i < selectedActivities.size(); i++) {
+//            System.out.print("A" + selectedActivities.get(i) + " ");
+//        }
+//        System.out.println();
+//    }
+//}
+////output
+////Max activities: 4
+////        A1 A2 A3 A5
