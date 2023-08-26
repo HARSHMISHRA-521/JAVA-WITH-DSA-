be3150df1527d992ea74696c5e812bad8b112288
HARSH <mishraharshmishra2020@gmail.com>
2023-08-26 19:35:33 +0530

    DSA CHAPTERWISE QUESTIONS

---
+++ CG00_MISCLLENEOUS/Kickdrum_Technical_Interview.java
@@ -0,0 +1,29 @@
+//package CG00_MISCLLENEOUS;
+//
+//public class interview {
+//
+//    public static int MaxArea(int arr[]){
+//        int max =Integer.MIN_VALUE;
+//        int allarea[] = new int[n];
+//        int n = arr.length;
+//        int dist =0;
+//        for(int i= 0;i< n;i++){
+//            int curr = i;
+//            for(int j = i+1;j<n;j++){
+//                dist ++;
+//                for(int k = curr;k< j;k++){
+//                    int hr = arr[curr];
+//                    int hl = arr[j];
+//                    int min = Math.min(hr,hl);
+//                    int area  =  min * dist;
+//                    allarea[curr] = area;
+//                }
+//            }
+//        }
+//        Arrays.sort(allarea);
+//        return allarea[n-1];
+//    }
+//    public static void main(String[] args) {
+//
+//    }
+//}
