6e174a548994954f5072a2b1be1116cbad1d4048
HARSH <mishraharshmishra2020@gmail.com>
2024-08-02 06:24:35 +0530

    adds the code

---
+++ CH21_BASIC_MATHAMATICS/SwapUsingXOR.java
@@ -0,0 +1,16 @@
+package CH21_BASIC_MATHAMATICS;
+
+public class SwapUsingXOR {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        System.out.println("Before swap: a = " + a + ", b = " + b);
+
+        // Swap logic
+        a = a ^ b;
+        b = a ^ b;
+        a = a ^ b;
+
+        System.out.println("After swap: a = " + a + ", b = " + b);
+    }
+}
